9172f5c8c3ab6a80658ef0f32e4d9b1f05b5fed4
Elaine Soares <elainehap@gmail.com>
2026-03-31 22:23:41 -0300

    Enviando a camada(model, repository) do Restaurante criada

---
+++ src/main/java/com/deliverytech/delivery_api/repository/RestauranteRepository.java
@@ -0,0 +1,15 @@
+package com.deliverytech.delivery_api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.deliverytech.delivery_api.model.Restaurante;
+
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+    List<Restaurante> findByCategoria(String categoria);
+    List<Restaurante> findByAtivoTrue();
+    
+}
+
